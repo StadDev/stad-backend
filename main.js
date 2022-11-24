1e983131a2020373e8c4c098c053a302d81d6cc0
daemon <paakwesinunoo135@gmail.com>
2022-11-23 22:12:56 -0800

    connected to database

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const express=require('express')
 const app=express()
 const dotenv = require('dotenv').config()
+const connectDb = require('./config/databaseConnect')
 
 
-//WELL DONE NUNUU
-//now create the folders needed
 
 const port=process.env.PORT  
 
+
+
+
+
+connectDb()
 app.listen(port,()=>{
     console.log(`port is running on ${port}`)
 })
