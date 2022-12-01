36d3cdd1f616da94f6504d01fdc64fa351eb8475
obirikan <obirikan020@gmail.com>
2022-12-01 13:19:42 +0000

    product handler

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ require('dotenv').config()
 const connectDb = require('./config/databaseConnect')
 const cors=require('cors')
 const userauth=require('./routes/userRoutes')
+const handler=require('./Handlers/Products')
 
 app.use(express.json())
 app.use(cors())
 app.use('/Apihandler/userauth',userauth)
+app.use('/Apihandler/handlers',handler)
 
 //for now lets nots give it a port in the env (secof hosting purposes)
 const port=process.env.PORT || 9000
